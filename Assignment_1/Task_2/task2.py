14e02cba68cca0e0db73cb57d8224251ffe95519
Yuanzhe0303 <148286016+Yuanzhe0303@users.noreply.github.com>
2025-05-20 13:23:15 -0700

    Denny Update task2.py

--- Assignment_1/Task_2/task2.py
+++ Assignment_1/Task_2/task2.py
@@ -2,12 +2,11 @@ import time
 import serial 
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-
+import matplotlib.ticker as ticker
 
 def animate(i, dataList, ser):
     ser.write(b'g')
     arduinoData_string = ser.readline().decode('ascii')
-    print(arduinoData_string)
 
     try:
         arduinoData_float = float(arduinoData_string)
@@ -20,27 +19,18 @@ def animate(i, dataList, ser):
     
     ax.clear()
     ax.plot(dataList)
-
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(10))
     ax.set_ylim([-90, 90])
-
+    ax.set_title("Arduino Data")
+    ax.set_ylabel("Value")
 
 dataList = []
 
 fig  = plt.figure()
 ax = fig.add_subplot(111)
 
-ax.set_title('Azimuth Angle Reading vs. Time')
-ax.set_xlabel('Time (s)')
-ax.set_ylabel('Azimuth Angle (degrees)')
-
-ser = serial.Serial("/dev/ttyACM0", 115200)
+ser = serial.Serial("COM3", 9600)
 time.sleep(2)
-ser.write(b'g')
 
-
-while True:
-    
-    ani = animation.FuncAnimation(fig, animate, frames=100, fargs=(dataList, ser), interval = 100)
-
-    plt.show()
-    ser.close()
+ani = animation.FuncAnimation(fig, animate, frames=100, fargs=(dataList, ser), interval = 100)
+plt.show()
