2092c66d3ae2981329f6c67911264a3c13636fdc
Qurqi <125725390+Qurqi@users.noreply.github.com>
2025-05-20 15:59:37 -0700

    Update task4.ino

--- Assignment_1/Task_4/task4.ino
+++ Assignment_1/Task_4/task4.ino
@@ -3,7 +3,10 @@
 char buffer[20];
 char userInput;
 float k = 0.01;
-float G_angle[2] = {0, 0};
+float W_angle[2] = {0,0};
+float G_angle = 0;
+float rate;
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -13,12 +16,15 @@ void setup() {
     Serial.println("Failed to initialize IMU!");
     while (1);
   }
+  rate = IMU.gyroscopeSampleRate();
+  rate = (1/rate);
+
+
   
 }
-
 void loop() {
 
-  float Ax, Ay, Az, Gx, Gy, Gz, A_angle, W_angle;
+  float Ax, Ay, Az, Gx, Gy, Gz, A_angle;
 
   // check for user input 
   if (Serial.available() > 0) {
@@ -39,29 +45,29 @@ void loop() {
           if (IMU.gyroscopeAvailable()) {
                 IMU.readGyroscope(Gx, Gy, Gz);
                 // read the sampling rate
-                float rate = IMU.gyroscopeSampleRate();
                 // calculate the angle
                 // replace G_angle[0] with G_angle[1], then replace G_angle[1] with Gy
-                G_angle[0] = G_angle[1];
-                G_angle[1] = G_angle[0] + Gy*(1/rate);
+                G_angle = G_angle + Gx*(rate);
           }
 
           // calculate weighted angle
-          W_angle = k*(G_angle[0]+G_angle[1]) + (1-k)*(A_angle);
+
+          // update previous angle reading
+          W_angle[1] = k*(W_angle[0]+(G_angle*-1)) + (1-k)*(A_angle);
 
           // pack the buffer with colon-delimited angle values
-          sprintf(buffer, "%.2f:%.2f:%.2f", A_angle, G_angle[1], W_angle);
+          sprintf(buffer, "%.2f:%.2f:%.2f", A_angle, (G_angle*-1), W_angle[1]);
           // send the buffer to the serial port
           Serial.println(buffer);
+          //W_angle[0] = W_angle[1];
+
           // clear the buffer 
           memset(buffer, 0, sizeof(buffer));
       }
       // if user input is 'n' iterate k-value by 0.01
       if (userInput == 'n') {
           k += 0.01;
-          if (k > 1) {
-              k = 1;
-          }
+          
       }
       // if user input is 'e' exit
       if (userInput == 'e'){
