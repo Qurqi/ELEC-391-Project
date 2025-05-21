ed36f78bf45e1d92cea71d87868132aac39423e0
Yuanzhe0303 <148286016+Yuanzhe0303@users.noreply.github.com>
2025-05-21 16:23:06 -0700

    Denny Update task4.ino

--- Assignment_1/Task_4/task4.ino
+++ Assignment_1/Task_4/task4.ino
@@ -2,12 +2,13 @@
 
 char buffer[20];
 char userInput;
-float k = 0.62;
+float k = 0.82;
 float W_angle[2] = {0,0};
 float G_angle = 0;
 float NG_angle = 0;
 float rate;
-
+float NN_angle;
+float G_angleC=0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -28,24 +29,26 @@ void loop() {
 
       userInput = Serial.read();
       // if user input is 'k' send k value
-
+      if (IMU.accelerationAvailable()) {
+              IMU.readAcceleration(Ax, Ay, Az);
+      }
       if (IMU.gyroscopeAvailable()) {
                 IMU.readGyroscope(Gx, Gy, Gz);
                 // read the sampling rate
                 // calculate the angle
                 // replace G_angle[0] with G_angle[1], then replace G_angle[1] with Gy
-                G_angle = G_angle + Gx*(rate);
+                G_angle = Gx*(rate);
+                G_angleC = G_angleC + G_angle;
           }
 
-      /*if (IMU.accelerationAvailable()) {
-              IMU.readAcceleration(Ax, Ay, Az);
-              A_angle = atan(Ay/Az)*180/3.1415926;
-      }
+      A_angle = atan(Ay/Az)*180/3.1415926;
       // calculate weighted angle
       // update previous angle reading
-      NG_angle = G_angle*-1;
-      W_angle[1] = k*(W_angle[0]+NG_angle) + (1-k)*(A_angle);
-      */
+      NG_angle = -1*G_angle;
+      NN_angle = -1*G_angleC;
+      W_angle[1] = k*(W_angle[1]+NG_angle) + (1-k)*(A_angle);
+      
+      sprintf(buffer, "%.2f:%.2f:%.2f", A_angle, NN_angle, W_angle[1]);
 
 
       if (userInput == 'k') {
@@ -55,23 +58,14 @@ void loop() {
       // if user input is 'd' return angle data
       if (userInput == 'd') {
           // pack the buffer with colon-delimited angle values
-        if (IMU.accelerationAvailable()) {
-          IMU.readAcceleration(Ax, Ay, Az);
-          A_angle = atan(Ay/Az)*180/3.1415926;
-        }
-        // calculate weighted angle
-        // update previous angle reading
-        NG_angle = G_angle*-1;
-        W_angle[1] = k*(W_angle[0]+NG_angle) + (1-k)*(A_angle);
-
-        sprintf(buffer, "%.2f:%.2f:%.2f", A_angle, NG_angle, W_angle[1]);
+        //sprintf(buffer, "%.2f:%.2f:%.2f", A_angle, NG_angle, W_angle[1]);
         // send the buffer to the serial port
           
         Serial.println(buffer);
         
           
         // clear the buffer 
-        memset(buffer, 0, sizeof(buffer));
+        //memset(buffer, 0, sizeof(buffer));
       }
       // if user input is 'n' iterate k-value by 0.01
       if (userInput == 'n') {
@@ -88,4 +82,3 @@ void loop() {
   
   
 }
-
